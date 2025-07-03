34fd8e1f4339bf148d253d5aa6bfe342abbd12b0
Parag Moagarkar <147545358+ParagMogarkar@users.noreply.github.com>
2025-07-03 09:03:11 +0530

    Create IMG_Encryption.py

---
+++ IMG_Encryption.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+def encrypt_image(path, key):
+    img = Image.open(path)
+    pixels = img.load()
+
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r, g, b = pixels[i, j]
+            pixels[i, j] = ((r + key) % 256, (g + key) % 256, (b + key) % 256)
+
+    img.save("encrypted.png")
+
+def decrypt_image(path, key):
+    img = Image.open(path)
+    pixels = img.load()
+
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            r, g, b = pixels[i, j]
+            pixels[i, j] = ((r - key) % 256, (g - key) % 256, (b - key) % 256)
+
+    img.save("decrypted.png")
